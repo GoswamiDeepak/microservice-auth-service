dbde317a189417c4eb745077d85bad50593f533d
Goswami Deepak Kumar <43085937+GoswamiDeepak@users.noreply.github.com>
2024-10-21 00:22:20 +0530

    add winston logger

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,7 @@ export default [
         },
         rules: {
             'no-unused-vars': 'error',
-            "no-console" : "error"
+            'no-console': 'error',
         },
     },
     pluginJs.configs.recommended,
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,19 @@
-function login(username :string): string {
-    
-    return username;
-}
-login("deepak");
+import app from './app';
+import { Config } from './config';
+import logger from './config/logger';
+
+const startServer = () => {
+    try {
+        app.listen(Config.PORT, () => {
+            logger.info(`Server is running on port ${Config.PORT}`);
+        });
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+            logger.error(error.message);
+        }
+        setTimeout(() => {
+            process.exit(1);
+        }, 1000);
+    }
+};
+startServer();
