cd0dcd8620040f3c35c34e2de4cbe300a68a713c
Goswami Deepak Kumar <43085937+GoswamiDeepak@users.noreply.github.com>
2024-10-30 03:12:36 +0530

    add test case login

--- tests/user/login.spec.ts
+++ tests/user/login.spec.ts
@@ -1,12 +1,17 @@
 import { DataSource } from 'typeorm';
+import bcrypt from 'bcrypt';
+import request from 'supertest';
 import { AppDataSource } from '../../src/config/data-source';
+import app from '../../src/app';
+import { isJwt } from '../utils';
+import { User } from '../../src/entity/User';
+import { Role } from '../../src/constants';
+import { App } from 'supertest/types';
 
 describe('POST /auth/login', () => {
-    //Database connection
     let connection: DataSource;
 
     beforeAll(async () => {
-        //connect database
         connection = await AppDataSource.initialize();
     });
 
@@ -16,10 +21,89 @@ describe('POST /auth/login', () => {
     });
 
     afterAll(async () => {
-        //disconnect database
         await connection.destroy();
     });
 
-    describe('Given all fields', async () => {});
-    describe('Missing fields', async () => {});
+    describe('Given all fields', () => {
+        it('should return the access token and refresh token inside a cookie', async () => {
+            // Arrange
+            const userData = {
+                firstname: 'Rakesh',
+                lastname: 'K',
+                email: 'rakesh@mern.space',
+                password: 'password',
+            };
+
+            const hashedPassword = await bcrypt.hash(userData.password, 10);
+
+            const userRepository = connection.getRepository(User);
+            //register user
+            await userRepository.save({
+                ...userData,
+                password: hashedPassword,
+                role: Role.CUSTOMER,
+            });
+
+            // Act
+            const response = await request(app as unknown as App)
+                .post('/auth/login')
+                .send({ email: userData.email, password: userData.password });
+
+            interface Headers {
+                ['set-cookie']: string[];
+            }
+            // Assert
+            let accessToken = null;
+            let refreshToken = null;
+            const cookies =
+                (response.headers as unknown as Headers)['set-cookie'] || [];
+            // eslint-disable-next-line no-console
+            console.log(cookies);
+            cookies.forEach((cookie) => {
+                if (cookie.startsWith('accessToken=')) {
+                    accessToken = cookie.split(';')[0].split('=')[1];
+                    // eslint-disable-next-line no-console
+                    console.log({ accessToken });
+                }
+
+                if (cookie.startsWith('refreshToken=')) {
+                    refreshToken = cookie.split(';')[0].split('=')[1];
+                    // eslint-disable-next-line no-console
+                    console.log({ refreshToken });
+                }
+            });
+            expect(accessToken).not.toBeNull();
+            expect(refreshToken).not.toBeNull();
+
+            expect(isJwt(accessToken)).toBeTruthy();
+            expect(isJwt(refreshToken)).toBeTruthy();
+        });
+        it('should return the 400 if email or password is wrong', async () => {
+            // Arrange
+            const userData = {
+                firstname: 'Rakesh',
+                lastname: 'K',
+                email: 'rakesh@mern.space',
+                password: 'password',
+            };
+
+            const hashedPassword = await bcrypt.hash(userData.password, 10);
+
+            const userRepository = connection.getRepository(User);
+            await userRepository.save({
+                ...userData,
+                password: hashedPassword,
+                role: Role.CUSTOMER,
+            });
+
+            // Act
+            const response = await request(app as unknown as App)
+                .post('/auth/login')
+                .send({ email: userData.email, password: 'wrongPassword' });
+
+            // Assert
+
+            expect(response.statusCode).toBe(400);
+        });
+    });
 });
