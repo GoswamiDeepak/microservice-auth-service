ed8d35ca75b3d973475cc93dc4b083b4469e4dec
Goswami Deepak Kumar <43085937+GoswamiDeepak@users.noreply.github.com>
2024-10-29 12:53:52 +0530

    add generatekey file

--- scripts/generateKeys.mjs
+++ scripts/generateKeys.mjs
@@ -1,7 +1,7 @@
 import crypto from 'crypto';
 import fs from 'fs';
 
- const {privateKey, publicKey}=crypto.generateKeyPairSync('rsa',{
+const { privateKey, publicKey } = crypto.generateKeyPairSync('rsa', {
     modulusLength: 2048,
     publicKeyEncoding: {
         type: 'pkcs1',
@@ -10,12 +10,12 @@ import fs from 'fs';
     privateKeyEncoding: {
         type: 'pkcs1',
         format: 'pem',
-    }
-})
+    },
+});
 // eslint-disable-next-line no-console
-console.log('public Key',publicKey);
+console.log('public Key', publicKey);
 // eslint-disable-next-line no-console
-console.log('private Key',privateKey);
+console.log('private Key', privateKey);
 
-fs.writeFileSync('certs/private.pem', privateKey)
-fs.writeFileSync('certs/public.pem', publicKey)
+fs.writeFileSync('certs/private.pem', privateKey);
+fs.writeFileSync('certs/public.pem', publicKey);
