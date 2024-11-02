075d97f404fe01fc7743349aca6477d98cbb278d
Goswami Deepak Kumar <43085937+GoswamiDeepak@users.noreply.github.com>
2024-11-03 00:53:03 +0530

    add  test for exclude password field

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -161,6 +161,6 @@ export class AuthController {
 
     async self(req: AuthRequest, res: Response) {
         const user = await this.userService.findbyId(Number(req.auth.sub));
-        res.status(200).json(user);
+        res.status(200).json({ ...user, password: undefined });
     }
 }
