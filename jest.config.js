f3aec8d1e37ae11cb4df16712d943de74a555d10
Goswami Deepak Kumar <43085937+GoswamiDeepak@users.noreply.github.com>
2024-11-19 11:15:02 +0530

    add format file

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
     testEnvironment: 'node',
-    testPathIgnorePatterns: ["/node_modules/", "/dist/"],
+    testPathIgnorePatterns: ['/node_modules/', '/dist/'],
     transform: {
         '^.+.tsx?$': ['ts-jest', {}],
     },
